b53e218aa67fb57baa3dbcc21972e311fbdc5b64
Howard <howard.zhang0108@gmail.com>
2026-07-23 16:37:25 -0400

    Add win game logic for rows, columns, and horizontals

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameboard = (() => {
-    let board = [["", "", ""], ["", "", ""], ["", "", ""]];
+    let board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
     return board;
 })();
 
@@ -9,5 +9,55 @@ function createPlayer(name, marker) {
 };
 
 function createGame() {
+    let score = 0;
+    let active = true;
 
-};
+    function getChoice(player) {
+        return prompt(player.name + "'s turn");
+    }
+
+    function checkGame() {
+        let rowWin = false;
+        let columnWin = false;
+        let horizontalWin = false;
+        board.forEach(row => {
+            if (row[0] === row[1] && row[0] === row[2]) {
+                rowWin = true;
+            }
+        for (let index = 0; index > 3; index++) {
+            if (board[0][index] === board[1][index] && board[0][index === board[2][index]])
+                columnWin = true;
+        }
+        if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+            horizontalWin = true;
+        }
+        if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+            horizontalWin = true;
+        }
+
+        if (rowWin || columnWin || horizontalWin) {
+            
+        }
+            
+        });
+    }
+
+
+
+    return {
+        playRound(playerOne, playerTwo) {
+            while (active) {
+                getChoice(playerOne);
+                checkGame();
+                getChoice(playerTwo);
+                checkGame();
+            }
+        },
+        getScore() {
+
+        },
+        reset() {
+
+        }
+    }
+}
