17b27733bd6357cce2b11b456af22b82abd7dffe
Howard <howard.zhang0108@gmail.com>
2026-07-28 17:53:57 -0400

    Add events for opening and closing change name modal

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createPlayer(name, marker) {
     return { name, marker, score: 0 };
 };
 
-function createGame(playerOne, playerTwo) {
+function createGame(playerOne = "Player 1", playerTwo = "Player 2") {
     let gameState = true;
     let checkGameCounter = 0;
 
@@ -92,3 +92,52 @@ function createGame(playerOne, playerTwo) {
         }
     }
 }
+
+function createDisplay() {
+    const openModalButtons = document.querySelectorAll('[data-modal-target]');
+    const closeModalButtons = document.querySelectorAll('[data-modal-close]');
+    const overlay = document.getElementById('overlay');
+
+    openModalButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const modal = document.querySelector(button.dataset.modalTarget);
+            openModal(modal);
+        })
+    })
+
+    closeModalButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const modal = button.closest('.modal')
+            closeModal(modal);
+        })
+    })
+
+    overlay.addEventListener('click', () => {
+        const modals = document.querySelectorAll(".modal.active");
+        modals.forEach(modal => {
+            closeModal(modal);
+        })
+    })
+
+    function openModal(modal) {
+        if (modal == null) return;
+        modal.classList.add("active");
+        overlay.classList.add("active");
+    }
+
+    function closeModal(modal) {
+        if (modal == null) return;
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+
+
+    return {
+        
+    }
+}
+
+display = createDisplay();
+
+
+
